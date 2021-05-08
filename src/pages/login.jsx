cea480a3c7ed0c9b2ddcb84e44631d860ca41a8c
Luiz Gustavo <luiz.gustavo@apiki.com>
2021-05-08 15:08:23 -0300

    refactor(login): removendo codigo desnecessario

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,17 +1,13 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { withSSRGuest } from '@utils/withSSRGuest';
 import LoginForm from '@components/layouts/LoginForm';
-import { AuthContext } from '@contexts/AuthContext';
 
 export const getServerSideProps = withSSRGuest(async () => ({
   props: {},
 }));
 
 function Login() {
-  const { isAuthenticated } = useContext(AuthContext);
-
-  console.log({ isAuthenticated });
   return <LoginForm />;
 }
 
