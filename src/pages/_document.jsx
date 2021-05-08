d52f23adbd2a9caa19232afd099e17e7460c8976
Luiz Gustavo <luiz.gustavo@apiki.com>
2021-05-08 20:51:06 -0300

    refactor(_document): importando fonte de peso 500

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -35,7 +35,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
