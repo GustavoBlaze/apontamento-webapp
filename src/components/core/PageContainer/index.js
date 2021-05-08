479f3f425058300940a6008ffdb5f205c33d0b0e
Luiz Gustavo <luiz.gustavo@apiki.com>
2021-05-08 20:42:14 -0300

    refactor(PageContainer): adicionando padding inferior

--- src/components/core/PageContainer/index.js
+++ src/components/core/PageContainer/index.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export default styled.div`
   width: 100%;
   max-width: 720px;
-  padding: 2.4rem 1.6rem 0;
+  padding: 2.4rem 1.6rem;
   margin: 0 auto;
 `;
