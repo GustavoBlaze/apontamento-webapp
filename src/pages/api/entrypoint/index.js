4216c91b821e137d1cece6ed12959964ddad45e2
Luiz Gustavo <luiz.gustavo@apiki.com>
2021-05-11 19:12:19 -0300

    refactor(api): formatando entrypoints para retornar

--- src/pages/api/entrypoint/index.js
+++ src/pages/api/entrypoint/index.js
@@ -1,5 +1,7 @@
 import { withAuth } from '@utils/withAuth';
 import prisma from '@lib/prisma';
+import groupBy from 'lodash/groupBy';
+import { format } from 'date-fns';
 
 async function handler(req, res) {
   if (req.method !== 'GET') return res.status(400).send('Verifique seus dados');
@@ -21,7 +23,23 @@ async function handler(req, res) {
     },
   });
 
-  return res.json(entryPoints);
+  const groupedByDayEntryPoints = groupBy(entryPoints, ({ createdAt }) =>
+    format(createdAt, 'dd/MM/yyyy'),
+  );
+
+  const arrayOfGroupedEntries = Object.keys(groupedByDayEntryPoints).map(
+    (key) => ({
+      date: key,
+      entries: [
+        ...groupedByDayEntryPoints[key].map(({ createdAt, ...rest }) => ({
+          ...rest,
+          createdAt: JSON.stringify(createdAt).replace(/"/g, ''),
+        })),
+      ],
+    }),
+  );
+
+  return res.json(arrayOfGroupedEntries);
 }
 
 export default withAuth(handler);
